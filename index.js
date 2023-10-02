e356d016edade5d049a4ab3913d88c85e7e2e4e4
choudhs2 <choudhs2@bxscience.edu>
2023-10-01 22:15:44 -0400

    added functionality to coloring cells and eventListeners to all cells on creation

--- index.js
+++ index.js
@@ -13,14 +13,16 @@ addRowButton.addEventListener("click", () => {
 
         cols += 1;
 
-        cell.setAttribute("bgcolor", "FFFFFF");
+        cell.setAttribute("bgcolor", "FFFFFF");//creates the cell with a default white color
+        cell.addEventListener("click", setColor); //creates a listener for each cell to set its color
     }
     // Add empty row
     else {
         for(let i = 0; i < cols; i++) {
             let cell = row.insertCell(i);
 
-            cell.setAttribute("bgcolor", "FFFFFF");
+            cell.setAttribute("bgcolor", "FFFFFF");//creates the cell with a default white color
+            cell.addEventListener("click", setColor); //creates a listener for each cell to set its color
         }
     }
 });
@@ -32,8 +34,8 @@ addColButton.addEventListener("click", () => {
         let row = table.insertRow(0);
         let cell = row.insertCell(0);
 
-        cell.setAttribute("bgcolor", "FFFFFF");
-
+        cell.setAttribute("bgcolor", "FFFFFF"); //creates the cell with a default white color
+            cell.addEventListener("click", setColor); //creates a listener for each cell to set its color
         rows += 1;
     }
     // Add empty column
@@ -41,8 +43,45 @@ addColButton.addEventListener("click", () => {
         // Loop through rows and APPEND the column cells to the right
         for(let i = 0; i < rows; i++) {
             let cell = table.rows[i].insertCell(cols);   // APPEND a cell to the right
-            cell.setAttribute("bgcolor", "FFFFFF");
+            cell.setAttribute("bgcolor", "FFFFFF"); //creates the cell with a default white color
+            cell.addEventListener("click", setColor); //creates a listener for each cell to set its color
         }
     }
     cols += 1;
+});
+
+function setColor(evt) {
+    const color = document.body.querySelector("#color-picker").value; //this is the current color of the color-picker
+    evt.target.setAttribute("bgcolor", color);
+}
+
+const allColorButton = document.body.querySelector("#color-all");
+allColorButton.addEventListener("click", () => {
+    const color = document.body.querySelector("#color-picker").value;
+    for(let i = 0; i < rows; i++) {
+        for(let j = 0; j < cols; j++) {
+            table.rows[i].cells[j].setAttribute("bgcolor", color);
+        }
+    }
+});
+
+const allUncoloredButton = document.body.querySelector("#color-uncolored");
+allUncoloredButton.addEventListener("click", () => {
+    const color = document.body.querySelector("#color-picker").value;
+    for(let i = 0; i < rows; i++) {
+        for(let j = 0; j < cols; j++) {
+            if(table.rows[i].cells[j].getAttribute("bgcolor") == "FFFFFF") {
+                table.rows[i].cells[j].setAttribute("bgcolor", color);
+            }
+        }
+    }
+});
+
+const clearAllButton = document.body.querySelector("#clear");
+clearAllButton.addEventListener("click", () => {
+    for(let i = 0; i < rows; i++) {
+        for(let j = 0; j < cols; j++) {
+            table.rows[i].cells[j].setAttribute("bgcolor", "FFFFFF");
+        }
+    }
 });
